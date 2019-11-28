4ca1126f628b0a6fe3ed9fd66b4ed7e34aeb51a1
liu <1373788543@qq.com>
2019-11-28 23:15:09 +0800

    有序数组的平方

---
+++ sortedSquares.java
@@ -0,0 +1,14 @@
+package sortedSquares;
+
+import java.util.Arrays;
+
+public class sortedSquares {
+    public int[] sortedSquares(int[] A) {
+        int[] arr = new int[A.length];
+        for (int i = 0; i < A.length; i++) {
+            arr[i] = A[i] * A[i];
+        }
+        Arrays.sort(arr);
+        return arr;
+    }
+}
